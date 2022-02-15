4add6d12dd980ff6f79a0448ae8a39d3f7a54a26
dev-araujo <theriancirculo@protonmail.com>
2022-02-15 15:44:43 -0300

    Base App Creation

---
+++ tarot_app/src/app/service/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private baseUrl: string =
+    'https://dentalclouddev.s3.amazonaws.com/challenge/tarot.json';
+
+
+  constructor(private api: HttpClient) {}
+
+  get list():Observable<any>{
+    return this.api.get<any>(this.baseUrl).pipe(tap((res) => res));
+  }
+
+
+}
