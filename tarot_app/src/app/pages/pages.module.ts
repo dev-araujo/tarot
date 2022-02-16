8c06d75e4bb11c572256010761e8d6d57193e71e
dev-araujo <theriancirculo@protonmail.com>
2022-02-16 08:56:17 -0300

    Home Creation

--- tarot_app/src/app/pages/pages.module.ts
+++ tarot_app/src/app/pages/pages.module.ts
@@ -5,9 +5,10 @@ import { RoutingModule } from './routing.module';
 
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PlayComponent],
   imports: [CommonModule, RoutingModule, SharedModule],
 })
 export class PagesModule {}
